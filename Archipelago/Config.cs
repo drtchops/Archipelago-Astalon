629fa6a0244a3b9c6a04afe62f3d46ff3baab973
Ian Robinson <ianronrobinson@gmail.com>
2024-02-25 02:16:48 -0500

    initial commit

---
+++ Archipelago/Config.cs
@@ -0,0 +1,12 @@
+namespace Archipelago;
+
+public class Config
+{
+    public string address { get; set; }
+    public int port { get; set; }
+    public string slotName { get; set; }
+    public string password { get; set; }
+
+    public bool invincibility { get; set; }
+    public bool maxDamage { get; set; }
+}
---
+++ Archipelago/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace Archipelago;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Astalon.exe")]
+public class Plugin : BasePlugin
+{
+    public static Config config;
+
+    public override void Load()
+    {
+        var configAddress = Config.Bind("General", "address", "archipelago.gg", "The address of the archipelago server to connect to");
+        var configPort = Config.Bind("General", "port", 38281, "The port of the archipelago server to connect to");
+        var configSlotName = Config.Bind("General", "slotName", "Player", "The slot name of the player you are connecting as");
+        var configPassword = Config.Bind("General", "password", "", "The password for the player you are connecting as");
+        var configInvincibility = Config.Bind("Cheats", "invincibility", false, "Whether the player character is invincible");
+        var configMaxDamage = Config.Bind("Cheats", "maxDamage", false, "Whether enemies always take 99 damage from attacks");
+
+        config = new Config
+        {
+            address = configAddress.Value,
+            port = configPort.Value,
+            slotName = configSlotName.Value,
+            password = configPassword.Value,
+            invincibility = configInvincibility.Value,
+            maxDamage = configMaxDamage.Value
+        };
+
+        APState.Connect();
+
+        var harmony = new Harmony("Archipelago");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} is loaded!");
+    }
+}
